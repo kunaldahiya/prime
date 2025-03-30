ee670614d2e9d3e4cef59f70b5c5ce8cd903f78a
kd <kunalsdahiya@gmail.com>
2025-03-30 17:12:49 +0530

    Added network class

---
+++ prime/models/network.py
@@ -0,0 +1,51 @@
+from torch import Tensor
+from torch.nn import Module
+from deepxml.models.modules import construct_module
+from deepxml.models.network import SiameseNetworkIS, _to_device
+from ._modules import MODS
+
+
+class SiameseNetworkIS(SiameseNetworkIS):
+    def _construct_module(self, config: str={}) -> Module:
+        if config is None:
+            return nn.Identity()
+        return construct_module(config, MODS)
+
+    def _encode_lbl(self, x: tuple) -> Tensor:
+        """Encode an item using the given network
+
+        Args:
+            x (tuple): #TODO
+
+        Returns:
+            torch.Tensor: Encoded item
+        """
+        return self.encoder_lbl(_to_device(x, self.device))
+
+    def encode_lbl(self, x: tuple, ind: Tensor) -> Tensor:
+        """Encode an item using the given network
+
+        Args:
+            x (tuple): #TODO
+
+        Returns:
+            torch.Tensor: Encoded item
+        """
+        _x = self._encode_lbl(x)
+        return self.transform_lbl((_x, ind)), _x
+
+    def forward(self, batch, *args):
+        """Forward pass
+
+        Args:
+            batch (dict): A dictionary containing features or 
+                tokenized representation and shared label shortlist
+
+        Returns:
+            torch.Tensor: output of the network (typically logits)
+        """
+        X = self.encode(_to_device(batch['X'], self.device))
+        Z, _Z = self.encode_lbl(
+            _to_device(batch['Z'], self.device), 
+            _to_device(batch['Y_s'], self.device))
+        return self.similarity(X, Z), self.similarity(X, _Z), X
