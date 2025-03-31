ee1a7cdb5c49df4ad61ceec7b97c81fbb01aebca
kd <kunalsdahiya@gmail.com>
2025-03-31 18:27:07 +0530

    Starting code for Model class

---
+++ prime/libs/model.py
@@ -0,0 +1,79 @@
+from typing import Callable
+from torch import Tensor
+
+import torch
+import numpy as np
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from deepxml.libs.model import EmbeddingModelIS as _EmbeddingModelIS
+
+
+class EmbeddingModelIS(_EmbeddingModelIS):
+    @torch.no_grad()
+    def _embeddings(
+        self,
+        data_loader: DataLoader,
+        encoder: Callable = None,
+        fname_out: str = None,
+        _dtype='float32'
+    ) -> np.ndarray:
+        """Encode given data points
+        * support for objects or files on disk
+
+
+        Args:
+            data_loader (DataLoader): DataLoader object to \
+                  create batches and iterate over it
+            encoder (Callable, optional): Defaults to None.
+                use this function to encode given dataset
+                * net.encode is used when None
+            fname_out (str, optional): dump features to this file. Defaults to None.
+            _dtype (str, optional): data type of output tensors. Defaults to 'float32'.
+
+        Returns:
+            np.ndarray: embeddings (as memmap or ndarray)
+        """
+
+        if encoder is None:
+            self.logger.info("Using the default encoder.")
+            encoder = self.net.encode
+        self.net.eval()
+        if fname_out is not None:  # Save to disk
+            embeddings = np.memmap(
+                fname_out, dtype=_dtype, mode='w+',
+                shape=(len(data_loader.dataset), self.net.repr_dims))
+        else:  # Keep in memory
+            embeddings = np.zeros((
+                len(data_loader.dataset), self.net.repr_dims),
+                dtype=_dtype)
+        idx = 0
+        for batch in tqdm(data_loader, desc="Computing Embeddings"):
+            bsz = batch['batch_size']
+            out = encoder(batch['X'], batch['indices'])
+            if isinstance(out, tuple):
+                out, _ = out
+            embeddings[idx :idx+bsz, :] = out.detach().cpu().numpy()
+            idx += bsz
+        torch.cuda.empty_cache()
+        if fname_out is not None:  # Flush all changes to disk
+            embeddings.flush()
+        return embeddings
+
+    def _setup_prototype_network(self, dataset):
+        raise NotImplementedError("")
+
+    def _setup(self, dataset):
+        self._init_memory_bank(dataset)
+        self._setup_prototype_network(dataset)
+
+    def _compute_loss(self, y_hat: tuple, batch: dict) -> Tensor:
+        """Compute loss
+
+        Args:
+            y_hat (tuple): predictions from the network
+            batch (dict): dict containing (local) ground truth
+
+        Returns:
+            Tensor: computed loss
+        """
+        raise NotImplementedError("")
